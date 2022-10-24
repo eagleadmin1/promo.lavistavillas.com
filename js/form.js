ceb9dac528250c6feb9c5b2e9ec9a4f9120f4073
Alexandr Kuzmin <kuzmin1913@gmail.com>
2022-10-24 13:54:07 +0300

    WIP

--- js/form.js
+++ js/form.js
@@ -7,12 +7,9 @@ $(document).ready(function () {
             },
             phone: {
                 required: true,
-            },
-            email: {
-                required: true,
             }
         },
-        submitHandler: function(form) {
+        submitHandler: function() {
             $.ajax({
                 type: 'POST',
                 headers: {
@@ -21,118 +18,58 @@ $(document).ready(function () {
                 url: 'https://dwh.villacarte.com/api/lead/create',
                 data: JSON.stringify(
                     {
-                        name: "Test LaVista",
-                        email: "test@test.com",
-                        phone: "+66696858854",
-                        language: "RU",
-                        comment: "комментарий",
-                        utmSource: "google",
-                        utmCampaign: "my_google_campaign",
-                        utmContent: "awsome_content",
-                        utmMedium: "cpc",
-                        utmTerm: "term",
-                        sourcePage: "promo.lavistavillas.com/ru",
-                        sourceForm: "Header form"
+                        name: $('#cf-top input[name=name]').val(),
+                        email: $('#cf-top input[name=email]').val(),
+                        phone: $('#cf-top input[name=phone]').val(),
+                        language: $('#cf-top input[name=language]').val(),
+                        sourcePage: window.location.href,
+                        sourceForm: "Top form"
                     }
                 ),
                 success() {
-                    alert("Success");
                     $("#cf-top .cf-step-1").hide();
                     $("#cf-top .cf-step-2").show();
                 },
-                error() {
-                    alert("Error");
-                }
-                // success:function(json){
-                //     alert("Success");
-                //     $("#cf-top .cf-step-1").hide();
-                //     $("#cf-top .cf-step-2").show();
-                // },
-                // error: function (error) {
-                //     alert("Error");
-                //     // console.log(error.response);
-                //     // var link = document.createElement('a');
-                //     // link.href = 'https://layangreenpark.com/agreement_sample_en.pdf';
-                //     // link.download = 'agreement_sample_en.pdf' 
-                //     // link.click();
-                    
-                    
-                // }  
             });
         }
     });
-    // $("#cf-top").on('submit', function (e){
-    //     e.preventDefault();
-    //     $.ajax({
-    //         type: 'POST',
-    //         url: 'https://dwh.villacarte.com/api/lead/create',
-    //         crossDomain: true,
-    //         data: JSON.stringify({ x: 5, y: 6 }),
-    //         dataType: 'json',
-    //         success:function(json){
-    //             alert("Success");
-    //         },
-    //         error:function(data){
-    //             var blob = new Blob([data]);
-    //             var link = document.createElement('a');
-    //             link.href = window.URL.createObjectURL(blob);
-    //             link.download = 'file:///Users/mac/Downloads/lavistavillas/agreement_sample_en.pdf';
-    //             link.click();
-                
-    //             $("#cf-top .cf-step-1").hide();
-    //             $("#cf-top .cf-step-2").show();
-    //         }  
-    //     });
-    // });
-    // $("#cf-top-mobile").on('submit', function (e){
-    //     e.preventDefault();
-    //     $.ajax({
-    //         type: 'POST',
-    //         url: 'https://dwh.villacarte.com/api/lead/create',
-    //         crossDomain: true,
-    //         data: JSON.stringify({ x: 5, y: 6 }),
-    //         dataType: 'json',
-    //         success:function(json){
-    //             alert("Success");
-    //         },
-    //         error:function(){
-    //             $("#cf-top-mobile .cf-step-1").hide();
-    //             $("#cf-top-mobile .cf-step-2").show();
-    //         }  
-    //     });
-    // });
-    // $("#cf-bottom").on('submit', function (e){
-    //     e.preventDefault();
-    //     $.ajax({
-    //         type: 'POST',
-    //         url: 'https://dwh.villacarte.com/api/lead/create',
-    //         crossDomain: true,
-    //         data: JSON.stringify({ x: 5, y: 6 }),
-    //         dataType: 'json',
-    //         success:function(json){
-    //             alert("Success");
-    //         },
-    //         error:function(){
-    //             $("#cf-bottom .cf-step-1").hide();
-    //             $("#cf-bottom .cf-step-2").show();
-    //         }  
-    //     });
-    // });
-    // $("#cf-bottom-mobile").on('submit', function (e){
-    //     e.preventDefault();
-    //     $.ajax({
-    //         type: 'POST',
-    //         url: 'https://dwh.villacarte.com/api/lead/create',
-    //         crossDomain: true,
-    //         data: JSON.stringify({ x: 5, y: 6 }),
-    //         dataType: 'json',
-    //         success:function(json){
-    //             alert("Success");
-    //         },
-    //         error:function(){
-    //             $("#cf-bottom-mobile .cf-step-1").hide();
-    //             $("#cf-bottom-mobile .cf-step-2").show();
-    //         }  
-    //     });
-    // });
+    $("#cf-bottom").validate({
+        errorPlacement: function() {},
+        rules: {
+            name: {
+                required: true,
+            },
+            phone: {
+                required: true,
+            }
+        },
+        submitHandler: function() {
+            $.ajax({
+                type: 'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                url: 'https://dwh.villacarte.com/api/lead/create',
+                data: JSON.stringify(
+                    {
+                        name: $('#cf-bottom input[name=name]').val(),
+                        email: $('#cf-bottom input[name=email]').val(),
+                        phone: $('#cf-bottom input[name=phone]').val(),
+                        language: $('#cf-bottom input[name=language]').val(),
+                        sourcePage: window.location.href,
+                        sourceForm: "Bottom form"
+                    }
+                ),
+                success() {
+                    $("#cf-bottom .cf-step-1").hide();
+                    $("#cf-bottom .cf-step-2").show();
+
+                    var link = document.createElement('a');
+                    link.href = 'https://eagleadmin1.github.io/promo.lavistavillas.com/agreement_sample_en.pdf';
+                    link.download = 'agreement_sample_en.pdf' 
+                    link.click();
+                },
+            });
+        }
+    });
 });
