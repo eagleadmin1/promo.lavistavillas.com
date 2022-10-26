c802c3ff12d78e22379c3e7609184d69b0bc66f3
Alexandr Kuzmin <kuzmin1913@gmail.com>
2022-10-26 13:42:23 +0300

    WIP

--- js/common.js
+++ js/common.js
@@ -172,3 +172,14 @@ $('#lvv-videoblock-btn-paly').click(function() {
     "*"
   );
 });
+
+$(".accordion__title").on("click", function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  if (!$this.hasClass("accordion-active")) {
+    $(".accordion__content").slideUp(400);
+    $(".accordion__title").removeClass("accordion-active");
+  }
+  $this.toggleClass("accordion-active");
+  $this.next().slideToggle();
+});
