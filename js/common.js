f19e8e8c629f30007cf48f26699263ff0a823c5b
Alexander Kuzmin <kuzmin1913@gmail.com>
2023-11-14 10:28:38 +0200

    Sceleton loading for sliders

--- js/common.js
+++ js/common.js
@@ -141,7 +141,32 @@ $('#lvv-location-carousel').flickity({
         x1: 60, y1: 50,
         x2: 70, y2: 50,
         x3: 35
-    }
+    },
+    on: {
+      ready: function () {
+        $('#lvv-location-carousel').removeClass('loading');
+      },
+    },
+});
+
+$('#lvv-development-progress').flickity({
+  cellAlign: 'center',
+  contain: true,
+  wrapAround: true,
+  pageDots: false,
+  selectedAttraction: 0.02,
+  imagesLoaded: true,
+  arrowShape: { 
+    x0: 15,
+    x1: 55, y1: 40,
+    x2: 65, y2: 30,
+    x3: 35
+  },
+  on: {
+    ready: function () {
+      $('#lvv-development-progress').removeClass('loading');
+    },
+  },
 });
 
 $('.lvv-villas-carousel-1').flickity({
@@ -209,21 +234,6 @@ setTimeout( () => {
     }
   });
 }, 1500);
-
-$('#lvv-development-progress').flickity({
-  cellAlign: 'center',
-  contain: true,
-  wrapAround: true,
-  pageDots: false,
-  selectedAttraction: 0.02,
-  imagesLoaded: true,
-  arrowShape: { 
-    x0: 15,
-    x1: 55, y1: 40,
-    x2: 65, y2: 30,
-    x3: 35
-  }
-});
 
 $('a[data-modal]').click(function(event) {
   $(this).modal({
