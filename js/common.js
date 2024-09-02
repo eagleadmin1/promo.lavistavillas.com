0796cc21f1caaf807f051ba7395ce6caf1f99fcc
Alexandr Kuzmin <kuzmin1913@gmail.com>
2024-09-02 14:25:52 +0300

    Изменил отправку данных с форм

--- js/common.js
+++ js/common.js
@@ -4,85 +4,85 @@ var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('.lvv-header').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
+$(window).scroll(function(event) {
+  didScroll = true;
 });
 
 setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
+  var st = $(this).scrollTop();
 
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.lvv-header').removeClass('lvv-header-down').addClass('lvv-header-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.lvv-header').removeClass('lvv-header-up').addClass('lvv-header-down');
-        }
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $('.lvv-header').removeClass('lvv-header-down').addClass('lvv-header-up');
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $('.lvv-header').removeClass('lvv-header-up').addClass('lvv-header-down');
     }
-    
-    lastScrollTop = st;
+  }
+
+  lastScrollTop = st;
 }
 
 // smooth scroll to link
 $(function() {
   $('a.lvv-nav-item[href*="#"]').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-              $('html, body').animate({
-                  scrollTop: target.offset().top
-              }, 500);
-              return false;
-          }
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
       }
+    }
   });
 });
 
 // hover container lang menu
 $('.lang').hover(
-  function(){
-      $('.lang ul').stop().slideToggle(100);
+  function() {
+    $('.lang ul').stop().slideToggle(100);
   },
-  function(){
-      $('.lang ul').stop().slideToggle(100);  
+  function() {
+    $('.lang ul').stop().slideToggle(100);
   }
 );
 
 // click languages
-$('.lang ul li').on('click', function(){
-    //select lang and apply changes
-    $lang = $(this).text();
-    $('.lang .current-lang').text($lang);
+$('.lang ul li').on('click', function() {
+  //select lang and apply changes
+  $lang = $(this).text();
+  $('.lang .current-lang').text($lang);
 });
 
 $('#burger').click(function() {
-    $('#burger').toggleClass("activated");
-    $('body').toggleClass('body-overflow-hidden');
-    $('#tablet-menu').toggleClass('show');
+  $('#burger').toggleClass("activated");
+  $('body').toggleClass('body-overflow-hidden');
+  $('#tablet-menu').toggleClass('show');
 });
 
 $('#tablet-menu .lvv-nav-item, #tablet-menu .lvv-btn-accent, .tablet-menu-overlay').click(function() {
-    $('#burger').toggleClass("activated");
-    $('#tablet-menu').toggleClass('show');
-    $('body').toggleClass('body-overflow-hidden');
+  $('#burger').toggleClass("activated");
+  $('#tablet-menu').toggleClass('show');
+  $('body').toggleClass('body-overflow-hidden');
 });
 
-$('#collapse-title-1').click(function(){
+$('#collapse-title-1').click(function() {
   $(this).toggleClass('show');
   $('.collapse-item-1').slideToggle('slow');
 });
@@ -132,24 +132,26 @@ function changeSlideClasses(slide, method, className) {
 }
 
 $('#lvv-location-carousel').flickity({
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-    pageDots: false,
-    selectedAttraction: 0.02,
-    imagesLoaded: true,
-    arrowShape: { 
-        x0: 25,
-        x1: 60, y1: 50,
-        x2: 70, y2: 50,
-        x3: 35
+  cellAlign: 'center',
+  contain: true,
+  wrapAround: true,
+  pageDots: false,
+  selectedAttraction: 0.02,
+  imagesLoaded: true,
+  arrowShape: {
+    x0: 25,
+    x1: 60,
+    y1: 50,
+    x2: 70,
+    y2: 50,
+    x3: 35
+  },
+  on: {
+    ready: function() {
+      $('#lvv-location-carousel').removeClass('loading');
+      $('#lvv-location-carousel').resize();
     },
-    on: {
-      ready: function () {
-        $('#lvv-location-carousel').removeClass('loading');
-        $('#lvv-location-carousel').resize();
-      },
-    },
+  },
 });
 
 $('#lvv-development-progress').flickity({
@@ -159,89 +161,42 @@ $('#lvv-development-progress').flickity({
   pageDots: false,
   selectedAttraction: 0.02,
   imagesLoaded: true,
-  arrowShape: { 
+  arrowShape: {
     x0: 15,
-    x1: 55, y1: 40,
-    x2: 65, y2: 30,
+    x1: 55,
+    y1: 40,
+    x2: 65,
+    y2: 30,
     x3: 35
   },
   on: {
-    ready: function () {
+    ready: function() {
       $('#lvv-development-progress').removeClass('loading');
       $('#lvv-development-progress').resize();
     },
   },
 });
 
-$('.lvv-villas-carousel-1').flickity({
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-    autoPlay: 4000,
-    imagesLoaded: true,
-    lazyLoad: 1,
-    arrowShape: { 
-      x0: 15,
-      x1: 55, y1: 40,
-      x2: 65, y2: 30,
-      x3: 35
-    }
+$('.lvv-villas-carousel').flickity({
+  cellAlign: 'center',
+  contain: true,
+  wrapAround: true,
+  autoPlay: 4000,
+  imagesLoaded: true,
+  lazyLoad: 1,
+  arrowShape: {
+    x0: 15,
+    x1: 55,
+    y1: 40,
+    x2: 65,
+    y2: 30,
+    x3: 35
+  }
 });
-
-setTimeout( () => {
-  $('.lvv-villas-carousel-2').flickity({
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-    autoPlay: 4000,
-    imagesLoaded: true,
-    lazyLoad: 1,
-    arrowShape: { 
-      x0: 15,
-      x1: 55, y1: 40,
-      x2: 65, y2: 30,
-      x3: 35
-    }
-  });
-}, 500);
-
-setTimeout( () => {
-  $('.lvv-villas-carousel-3').flickity({
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-    autoPlay: 4000,
-    imagesLoaded: true,
-    lazyLoad: 1,
-    arrowShape: { 
-      x0: 15,
-      x1: 55, y1: 40,
-      x2: 65, y2: 30,
-      x3: 35
-    }
-  });
-}, 1000);
-
-setTimeout( () => {
-  $('.lvv-villas-carousel-4').flickity({
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-    autoPlay: 4000,
-    imagesLoaded: true,
-    lazyLoad: true,
-    arrowShape: { 
-      x0: 15,
-      x1: 55, y1: 40,
-      x2: 65, y2: 30,
-      x3: 35
-    }
-  });
-}, 1500);
 
 $('a[data-modal]').click(function(event) {
   $(this).modal({
-    blockerClass: "lvv-contact-form-overlay", 
+    blockerClass: "lvv-contact-form-overlay",
     modalClass: "lvv-contact-form-modal",
     showClose: false,
     fadeDuration: 400
@@ -271,12 +226,12 @@ $(".accordion__title").on("click", function(e) {
   $this.next().slideToggle();
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
 
   jQuery.validator.addMethod("phone", function(phone_number, element) {
-      phone_number = phone_number.replace(/\s+/g, "");
+    phone_number = phone_number.replace(/\s+/g, "");
 
-      return this.optional(element) || phone_number.match(/^(\s*)?(\+)?([-()]?\d[-()]?){9,14}(\s*)?$/);
+    return this.optional(element) || phone_number.match(/^(\s*)?(\+)?([-()]?\d[-()]?){9,14}(\s*)?$/);
   }, '');
 
   const rules = {
@@ -293,109 +248,99 @@ $(document).ready(function () {
     }
   }
 
-  const getUrlParameter = function getUrlParameter(sParam) {
-      var sPageURL = window.location.search.substring(1),
-          sURLVariables = sPageURL.split('&'),
-          sParameterName,
-          i;
-  
-      for (i = 0; i < sURLVariables.length; i++) {
-          sParameterName = sURLVariables[i].split('=');
-  
-          if (sParameterName[0] === sParam) {
-              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-          }
+  const getUrlParameter = (sParam) => {
+    var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
       }
-      return '';
+    }
+    return '';
   };
 
-  function ajaxRequest (formId, sourceForm, downloadPdf = false) {
-      return(
-          $.ajax({
-              type: 'POST',
-              headers: {
-                  'Content-Type':'application/json'
-              },
-              url: 'https://villacarte.com/api/leads',
-              data: JSON.stringify(
-                  {
-                      leadType: 'SALE',
-                      name: $(`${formId} input[name=name]`).val(),
-                      email: $(`${formId} input[name=email]`).val(),
-                      phone: $(`${formId} input[name=phone]`).val(),
+  // Получает Google Client ID из cookie
+  const getGoogleClientID = () => {
+    const match = document.cookie.match('(?:^|;)\\s*_ga=([^;]*)'); // Ищет значение _ga
+    if (!match) return ''; // Если не найдено, возвращаем пустую строку
 
-                      utmSource: getUrlParameter('utm_source'),
-                      utmCampaign: getUrlParameter('utm_campaign'),
-                      utmContent: getUrlParameter('utm_content'),
-                      utmMedium: getUrlParameter('utm_medium'),
-                      utmTerm: getUrlParameter('utm_term'),
+    const raw = decodeURIComponent(match[1]); // Декодируем значение cookie
+    const clientIdMatch = raw.match(/(\d+\.\d+)$/); // Ищем клиентский ID в формате "GA1.2.xxxx.xxxx"
 
-                      sourcePage: window.location.href,
-                      sourceForm: sourceForm,
+    return clientIdMatch ? clientIdMatch[1] : ''; // Возвращаем найденный ID или пустую строку
+  };
 
-                      comments: 'LaVista',
-                  }
-              ),
-              success() {
-                  $(`${formId} .cf-step-1`).hide();
-                  $(`${formId} .cf-step-2`).show();
+  // Получает Yandex Client ID из cookie
+  const getYandexClientID = () => {
+    const match = document.cookie.match('(?:^|;)\\s*_ym_uid=([^;]*)'); // Ищет значение _ym_uid
+    return match ? decodeURIComponent(match[1]) : ''; // Возвращает найденное значение или пустую строку
+  };
 
-                  if(downloadPdf) {
-                      var link = document.createElement('a');
-                      link.href = $(`${formId} input[name=pdfUrl]`).val();
-                      link.download = $(`${formId} input[name=pdfName]`).val();
-                      link.click();
-                  }
-             
-                  ym(90929167,'reachGoal','submitted')
-                  gtag('event', 'Отправка формы', 
-                      { 
-                          'event_category': 'Form', 
-                          'event_action': 'submitted', }
-                  ); 
-              },
-          })
-      )
-  }
-
-  $('#cf-top').validate({
-      errorPlacement: function() {},
-      rules,
-      submitHandler: function() {
-          ajaxRequest('#cf-top', 'Top form');
-      }
-  });
-
-  $('#cf-top-mobile').validate({
-      errorPlacement: function() {},
-      rules,
-      submitHandler: function() {
-          ajaxRequest('#cf-top-mobile', 'Top form modal');
-      }
-  });
-
-  $('#cf-middle').validate({
-      errorPlacement: function() {},
-      rules,
-      submitHandler: function() {
-          ajaxRequest('#cf-middle', 'Middle form modal');
-      }
-  });
-
-  $('#cf-bottom').validate({
-      errorPlacement: function() {},
-      rules,
-      submitHandler: function() {
-          ajaxRequest('#cf-bottom', 'Bottom form', true);
-      }
-  });
+  const ajaxRequest = (formId, downloadPdf = false) => {
+    return $.ajax({
+      type: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: 'https://layanverde.com/api/lead',
+      data: JSON.stringify({
+        leadOwner: 'Dinara',
+        project: 'La Vista Villas',
+        language: window.location.href.includes('ru') ? 'RU' : 'EN',
+        sourcePage: window.location.href,
+        sourceForm: 'main form',
+        googleClientId: getGoogleClientID(),
+        yandexClientID: getYandexClientID(),
   
-  $('#cf-bottom-mobile').validate({
+        utmSource: getUrlParameter('utm_source'),
+        utmCampaign: getUrlParameter('utm_campaign'),
+        utmContent: getUrlParameter('utm_content'),
+        utmMedium: getUrlParameter('utm_medium'),
+        utmTerm: getUrlParameter('utm_term'),
+  
+        name: $(`${formId} input[name=name]`).val(),
+        email: $(`${formId} input[name=email]`).val(),
+        phone: $(`${formId} input[name=phone]`).val(),
+      }),
+      success() {
+        $(`${formId} .cf-step-1`).hide();
+        $(`${formId} .cf-step-2`).show();
+  
+        if (downloadPdf) {
+          const link = document.createElement('a');
+          link.href = $(`${formId} input[name=pdfUrl]`).val();
+          link.download = $(`${formId} input[name=pdfName]`).val();
+          link.click();
+        }
+  
+        ym(90929167, 'reachGoal', 'submitted');
+        gtag('event', 'Отправка формы', {
+          'event_category': 'Form',
+          'event_action': 'submitted',
+        });
+      },
+    });
+  }
+  
+  const setupFormValidation = (formId, downloadPdf = false) => {
+    $(formId).validate({
       errorPlacement: function() {},
       rules,
       submitHandler: function() {
-          ajaxRequest('#cf-bottom-mobile', 'Bottom form modal', true);
+        ajaxRequest(formId, downloadPdf);
       }
-  });
+    });
+  }
+  
+  setupFormValidation('#cf-top');
+  setupFormValidation('#cf-top-mobile');
+  setupFormValidation('#cf-middle');
+  setupFormValidation('#cf-bottom', true);
+  setupFormValidation('#cf-bottom-mobile', true);
 
 });
